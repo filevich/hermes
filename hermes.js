a35f8564c8ad8bd77b288f2f0697b362ac7f30ce
J.P. Filevich <juan.filevich@fing.edu.uy>
2017-09-10 18:32:36 -0300

    typo

--- hermes.js
+++ hermes.js
@@ -1,39 +1,34 @@
-var focused        = 0
-    , isFullScreen = false
-    , videos       = document.getElementsByTagName("video") //htmlCollection
-      videos       = [].slice.call(videos)                  //Array
+let focused      = 0
+  , isFullScreen = false
+  , videos       = document.getElementsByTagName("video") //htmlCollection
+    videos       = [].slice.call(videos);                  //Array
 
 videos[0].playbackRate = 1.8;
 
 // focus listener
-document.addEventListener('click', function(event){
-  var e = event.target
-
-  if(e.tagName != 'VIDEO')
-  {
+document.addEventListener('click', (event) => {
+  let e = event.target;
+  if (e.tagName != 'VIDEO') {
     focused = -1;
     return;
   }
   // else: video clicked, but which one?
-  focused = videos.findIndex(function(event){
+  focused = videos.findIndex((event) => {
     return event.id == e.id;
   })
-
   //event.preventDefault()
   //togglePause(videos[focused])
-})
+});
 
 // keyboard shortcuts listener
-document.addEventListener('keydown', function(event){
-  if (focused == -1)
+document.addEventListener('keydown', (event) => {
+  if (focused == -1){
     return;
-
+  }
   event.preventDefault()
-  var key = event.code
-
+  let key = event.code;
   // else: video focused
-  switch(key)
-  {
+  switch(key) {
     case 'Space':
     case 'KeyK':
     case 'KeyS':
@@ -41,93 +36,92 @@ document.addEventListener('keydown', function(event){
       break;
     case 'ArrowLeft':
     case 'KeyA':
-      videos[focused].currentTime -= 5
+      videos[focused].currentTime -= 5;
       break;
     case 'ArrowRight':
     case 'KeyD':
-      videos[focused].currentTime += 5
+      videos[focused].currentTime += 5;
       break;
     case 'ArrowUp':
-      videos[focused].volume *= 1.25
+      videos[focused].volume *= 1.25;
       break;
     case 'ArrowDown':
-      videos[focused].volume /= 1.25
+      videos[focused].volume /= 1.25;
       break;
     case 'KeyJ':
-      videos[focused].currentTime -= 10
+      videos[focused].currentTime -= 10;
       break;
     case 'KeyL':
-      videos[focused].currentTime += 10
+      videos[focused].currentTime += 10;
       break;
     case 'KeyF':
-      toggleFullScreen(videos[focused])
+      toggleFullScreen(videos[focused]);
       break;
     case 'Escape':
-      exitFullScreen(videos[focused])
+      exitFullScreen(videos[focused]);
       break;
     case 'Comma':
       videos[focused].playbackRate /= 1.1;
-      console.log(videos[focused].playbackRate)
+      console.log(videos[focused].playbackRate);
       break;
     case 'Period':
       videos[focused].playbackRate *= 1.1;
-      console.log(videos[focused].playbackRate)
+      console.log(videos[focused].playbackRate);
       break;
   }
 })
 
-var togglePause = function(v)
-{
-  if (v.paused)
-    v.play()
-  else
-    v.pause()
+let togglePause = (v) => {
+  if (v.paused){
+    v.play();
+  } else {
+    v.pause();
+  }
 }
 
-var toggleFullScreen = function(v)
-{
+let toggleFullScreen = (v) => {
   // firefox
-  if (v.mozRequestFullScreen)
-  {
-    if (isFullScreen)
-      exitFullScreen(v)
-    else
-      v.mozRequestFullScreen()
+  if (v.mozRequestFullScreen) {
+    if (isFullScreen) {
+      exitFullScreen(v);
+    } else {
+      v.mozRequestFullScreen();
+    }
   }
   // chrome
   else // if (v.webkitRequestFullScreen)
   {
-    if (isFullScreen)
-      exitFullScreen(v)
-    else
-      v.webkitRequestFullScreen()
+    if (isFullScreen) {
+      exitFullScreen(v);
+    } else{
+      v.webkitRequestFullScreen();
+    }
   }
 }
 
-var exitFullScreen = function(v)
-{
-  if (v.exitFullscreen)
-    v.exitFullscreen()
-  else if (v.mozCancelFullScreen)
-    v.mozCancelFullScreen()
-  else if (v.webkitExitFullscreen)
-    v.webkitExitFullscreen()
+let exitFullScreen = (v) => {
+  if (v.exitFullscreen) {
+    v.exitFullscreen();
+  } else if (v.mozCancelFullScreen) {
+    v.mozCancelFullScreen();
+  } else if (v.webkitExitFullscreen) {
+    v.webkitExitFullscreen();
+  }
 }
 
 /* fullscreen */
-
-document.addEventListener("fullscreenchange", function () {
+document.addEventListener("fullscreenchange", () => {
     isFullScreen = !isFullScreen;
 }, false);
 
-document.addEventListener("mozfullscreenchange", function () {
+document.addEventListener("mozfullscreenchange", () => {
     isFullScreen = !isFullScreen;
 }, false);
 
-document.addEventListener("webkitfullscreenchange", function () {
+document.addEventListener("webkitfullscreenchange", () => {
     isFullScreen = !isFullScreen;
 }, false);
 
-document.addEventListener("msfullscreenchange", function () {
+document.addEventListener("msfullscreenchange", () => {
     isFullScreen = !isFullScreen;
 }, false);
